2263e6debd821ca9365825a1093d7ca84c24125e
Tatsuya Saito <twopackas@gmail.com>
2014-10-25 13:57:37 +0900

    Fixes routes config which has explicit method. refs #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 RedmineApp::Application.routes.draw do
-  match 'projects/:project_id/xlsx_export', :to => 'xlsx_export#index'
-  match 'xlsx_export', :to => 'xlsx_export#index'
+  get 'projects/:project_id/xlsx_export', :to => 'xlsx_export#index'
+  get 'xlsx_export', :to => 'xlsx_export#index'
 end
