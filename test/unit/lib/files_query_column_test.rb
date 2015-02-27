1b5eae00039a7afac81c237bc61170ea9aff950b
Tatsuya Saito <twopackas@gmail.com>
2015-02-27 20:37:03 +0900

    Fixes test of attachment issues because Redmine fixture is changed. refs #11 See http://www.redmine.org/projects/redmine/repository/revisions/13942

--- test/unit/lib/files_query_column_test.rb
+++ test/unit/lib/files_query_column_test.rb
@@ -19,17 +19,32 @@ class FilesQueryColumnTest < ActiveSupport::TestCase
 
   def test_that_issue_has_a_file_without_description
     issue_with_a_file = Issue.find(2)
+    expected = ""
+    if Redmine::VERSION::MAJOR <= 2 then
+      expected = "picture.jpg"
+    else
+      expected = "source.rb : This is a Ruby source file\n" +
+                 "picture.jpg"
+    end
 
-    assert_equal 'picture.jpg', @sut.value(issue_with_a_file)
+    assert_equal expected, @sut.value(issue_with_a_file)
   end
 
   def test_that_issue_has_files_with_description
     issue_with_a_file = Issue.find(3)
-    expected = "error281.txt\n" +
-               "source.rb : This is a Ruby source file\n" +
-               "changeset_iso8859-1.diff\n" +
-               "archive.zip\n" +
-               "changeset_utf8.diff"
+    expected = ""
+    if Redmine::VERSION::MAJOR <= 2 then
+      expected = "error281.txt\n" +
+                 "source.rb : This is a Ruby source file\n" +
+                 "changeset_iso8859-1.diff\n" +
+                 "archive.zip\n" +
+                 "changeset_utf8.diff"
+    else
+      expected = "error281.txt\n" +
+                 "changeset_iso8859-1.diff\n" +
+                 "archive.zip\n" +
+                 "changeset_utf8.diff"
+    end
 
     assert_equal expected, @sut.value(issue_with_a_file)
   end
