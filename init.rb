2c170456aa525e6d348e22a3a649a3aa474e58d6
Tatsuya Saito <twopackas@gmail.com>
2016-08-06 17:49:27 +0900

    Changes version to 0.1.3 fixes #23

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_xlsx_format_issue_exporter do
   name 'Redmine XLSX format issue exporter'
   author 'Tatsuya Saito'
   description 'This is Redmine plugin which exports issue list to XLSX format file.'
-  version '0.1.2'
+  version '0.1.3'
   url 'https://github.com/two-pack/redmine_xlsx_format_issue_exporter'
   author_url 'mailto:twopackas@gmail.com'
   requires_redmine :version_or_higher => '2.3'
