190622cd4044d5b1acbfa3ede7197153413ca4f5
Tatsuya Saito <twopackas@gmail.com>
2017-03-16 23:31:28 +0900

    Fixes a error with IssueController change of Redmine r16399. refs #31

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -29,6 +29,9 @@ module IssuesControllerPatch
         end
       end
 
+      if @issues.nil?
+        @issues = @query.issues(:limit => Setting.issues_export_limit.to_i)
+      end
       send_data(query_to_xlsx(@issues, @query, params), :type => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;', :filename => 'issues.xlsx')
     end
   end
