2006bb0474a6a53b15cd04da1633a81b26f21c04
Tatsuya Saito <twopackas@gmail.com>
2017-04-06 22:07:40 +0900

    Fixes how to patch IssuesController for changing order to run index method. refs #36

--- lib/issues_controller_patch.rb
+++ lib/issues_controller_patch.rb
@@ -3,42 +3,32 @@ require_dependency 'issues_controller'
 module IssuesControllerPatch
   include XlsxExportHelper
 
-  def self.included(base) # :nodoc:
-    base.send(:include, InstanceMethods)
-
-    base.class_eval do
-      alias_method_chain :index, :xlsx
+  def query_issues
+    options = {:order => sort_clause, :offset => @offset, :limit => @limit}
+    if (Redmine::VERSION::MAJOR <= 3) && (Redmine::VERSION::MINOR <= 3) && (Redmine::VERSION::BRANCH != 'devel') then
+      options.merge!({:include => [:assigned_to, :tracker, :priority, :category, :fixed_version]})
     end
+    @query.issues(options)
   end
 
-  module InstanceMethods
-    def query_issues
-      options = {:order => sort_clause, :offset => @offset, :limit => @limit}
-      if (Redmine::VERSION::MAJOR <= 3) && (Redmine::VERSION::MINOR <= 3) && (Redmine::VERSION::BRANCH != 'devel') then
-        options.merge!({:include => [:assigned_to, :tracker, :priority, :category, :fixed_version]})
+  def index
+    begin
+      return super
+    rescue ActionController::UnknownFormat => e
+      if params[:format] != 'xlsx'
+        raise e
       end
-      @query.issues(options)
     end
 
-    def index_with_xlsx
-      begin
-        return index_without_xlsx
-      rescue ActionController::UnknownFormat => e
-        if params[:format] != 'xlsx'
-          raise e
-        end
-      end
-
-      if @issues.nil?
-        @issues = @query.issues(:limit => Setting.issues_export_limit.to_i)
-      end
-      send_data(query_to_xlsx(@issues, @query, params), :type => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;', :filename => 'issues.xlsx')
+    if @issues.nil?
+      @issues = @query.issues(:limit => Setting.issues_export_limit.to_i)
     end
+    send_data(query_to_xlsx(@issues, @query, params), :type => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;', :filename => 'issues.xlsx')
   end
 end
 
 ActionDispatch::Reloader.to_prepare do
   unless IssuesController.included_modules.include?(IssuesControllerPatch)
-    IssuesController.send(:include, IssuesControllerPatch)
+    IssuesController.send(:prepend, IssuesControllerPatch)
   end
 end
