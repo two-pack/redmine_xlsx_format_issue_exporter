6beb13a93800352fb690f6c105250628cfd8fbb9
two-pack <twopackas@gmail.com>
2017-07-10 00:05:36 +0900

    Changes version to 0.1.5 and description for reporting spent time. refs #46

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_xlsx_format_issue_exporter do
   name 'Redmine XLSX format issue exporter'
   author 'Tatsuya Saito'
-  description 'This is Redmine plugin which exports issue list to XLSX format file.'
-  version '0.1.4'
+  description 'This is Redmine plugin which exports XLSX format file.'
+  version '0.1.5'
   url 'https://github.com/two-pack/redmine_xlsx_format_issue_exporter'
   author_url 'mailto:twopackas@gmail.com'
   requires_redmine :version_or_higher => '2.3'
