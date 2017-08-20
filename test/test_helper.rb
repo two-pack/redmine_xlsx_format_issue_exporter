755fbe2aba7327e74df6ef63639e230a208bd51c
two-pack <twopackas@gmail.com>
2017-08-21 07:12:15 +0900

    Adds gem capybara-screenshot to test_helper. refs #47

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,6 +13,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../../../test/test_helper")
 
 require "capybara/rails"
 require "capybara/poltergeist"
+require "capybara-screenshot"
 
 module RedmineXlsxFormatIssueExporter
   class ActionDispatch::IntegrationTest
