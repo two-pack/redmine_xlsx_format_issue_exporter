79b06a5faf37c3a553fccf97b04db12feda3b718
two-pack <twopackas@gmail.com>
2018-09-29 21:48:59 +0900

    Fixes wrong columns after XLSX export. refs #64

--- lib/redmine_xlsx_format_issue_exporter/issues_controller_patch.rb
+++ lib/redmine_xlsx_format_issue_exporter/issues_controller_patch.rb
@@ -3,6 +3,9 @@ module RedmineXlsxFormatIssueExporter
     include XlsxExportHelper
 
     def index
+      saved_column_names = session[:issue_query][:column_names] if session[:issue_query].present?
+      saved_column_names ||= session[:query][:column_names] if session[:query].present?
+
       begin
         return super
       rescue ActionController::UnknownFormat => e
@@ -15,6 +18,9 @@ module RedmineXlsxFormatIssueExporter
         @issues = @query.issues(:limit => Setting.issues_export_limit.to_i)
       end
       send_data(query_to_xlsx(@issues, @query, params), :type => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;', :filename => 'issues.xlsx')
+
+      session[:issue_query][:column_names] = saved_column_names if session[:issue_query].present?
+      session[:query][:column_names] = saved_column_names if session[:query].present?
     end
   end
 end
--- test/functional/issues_controller_test.rb
+++ test/functional/issues_controller_test.rb
@@ -31,6 +31,16 @@ class IssuesControllerTest < ActionController::TestCase
   include Redmine::I18n
   include RedmineXlsxFormatIssueExporter
 
+  def column_names
+    if session[:issue_query].present?
+      session[:issue_query][:column_names]
+    elsif session[:query].present?
+      session[:query][:column_names]
+    else
+      nil
+    end
+  end
+
   def setup
     User.current = nil
   end
@@ -230,5 +240,23 @@ class IssuesControllerTest < ActionController::TestCase
     rescue ActionController::UnknownFormat => e
       pass
     end
+  end
+
+  def test_index_xlsx_should_not_change_selected_columns
+    get :index, :params => {
+        :set_filter => 1,
+        :c => ["subject", "due_date"],
+        :project_id => "ecookbook"
+    }
+    assert_response :success
+    assert_equal [:subject, :due_date], column_names
+    get :index, :params => {
+        :set_filter => 1,
+        :c =>["all_inline"],
+        :project_id => "ecookbook",
+        :format => 'xlsx'
+    }
+    assert_response :success
+    assert_equal [:subject, :due_date], column_names
   end
 end
