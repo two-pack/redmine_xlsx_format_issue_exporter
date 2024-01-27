0ea3c1967d37e39e405de13e0f58da8655601b63
two-pack <twopackas@gmail.com>
2024-01-28 07:31:02 +0900

    Changes version to 0.2.1.

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_xlsx_format_issue_exporter do
   name 'Redmine XLSX format issue exporter'
   author 'Tatsuya Saito'
   description 'This is Redmine plugin which exports XLSX format file.'
-  version '0.2.0'
+  version '0.2.1'
   url 'https://github.com/two-pack/redmine_xlsx_format_issue_exporter'
   author_url 'mailto:twopackas@gmail.com'
   requires_redmine :version_or_higher => '4.2'
